39cd58aba30922e7502869efa2ca6ec50b956af3
Reyhan Naufal <reyhannaufal21@gmail.com>
2021-09-07 02:19:29 +0700

    chore: add redux, rx and redux obervable

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,19 @@
-import Cta from '@/components/Cta';
-import Header from '@/components/Header';
-import type { NextPage } from 'next';
 import Head from 'next/head';
+import { map, of } from 'rxjs';
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-    return (
-        <main>
-            <Head>
-                <title>Home</title>
-            </Head>
-            <Header />
-            <section className='bg-pink-50'>
-                <div>
-                    <Cta />
-                </div>
-            </section>
-        </main>
-    );
+   return (
+      <main>
+         <Head>
+            <title>Home</title>
+         </Head>
+         <section className='flex flex-col items-center justify-center h-screen space-y-2 text-white bg-gray-700'>
+            <h1 className='text-4xl font-bold'>Me learning Rx</h1>
+            <p>🤌</p>
+         </section>
+      </main>
+   );
 };
 
 export default Home;
