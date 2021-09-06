c3f5dfb56c38e7528ea764de346bb2d59a231aa3
Reyhan Naufal <reyhannaufal21@gmail.com>
2021-09-07 03:08:50 +0700

    feat: add eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,28 @@
+import * as React from 'react';
 import Head from 'next/head';
-import { map, of } from 'rxjs';
+import { interval, map, Observable, of, Subscription } from 'rxjs';
 import type { NextPage } from 'next';
 
+const observable$: Observable<number> = interval(1000);
+
 const Home: NextPage = () => {
+   const [state, setState] = React.useState<number>();
+
+   React.useEffect(() => {
+      return () => {
+         observable$.subscribe(setState);
+      };
+   });
+
    return (
       <main>
          <Head>
-            <title>Home</title>
+            <title>Me Learning RxJS</title>
          </Head>
-         <section className='flex flex-col items-center justify-center h-screen space-y-2 text-white bg-gray-700'>
+         <section className='flex flex-col items-center justify-center h-screen space-y-5 text-white bg-gray-900'>
             <h1 className='text-4xl font-bold'>Me learning Rx</h1>
             <p>🤌</p>
+            <p>Time: {state}</p>
          </section>
       </main>
    );
